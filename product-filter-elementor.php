dd722b8f5bc2b28da14faf78b41fd7e9be46189a
kamaltz <kamaltz.dena16@gmail.com>
2025-08-09 19:58:19 +0700

    better lah

--- product-filter-elementor.php
+++ product-filter-elementor.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Product Filter Elementor
  * Description: Advanced Elementor widgets for product filtering and display with WooCommerce integration
- * Version: 2.6.6
+ * Version: 2.6.9
  * Author: kamaltz
  */
 
